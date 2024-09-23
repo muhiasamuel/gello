6ce4541dab1136641dbdba7e3564aac4b72cf14a
Grace <wangarigrace198@gmail.com>
2024-09-23 12:39:29 +0300

    puppeteer

--- src/app/container/product-details/highlights.directive.ts
+++ src/app/container/product-details/highlights.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlights]'
 })
 export class HighlightsDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
+  OnMouseEnter(){
+    
+  }
 }
